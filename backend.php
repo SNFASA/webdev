84427c54aa771e172c6081ce5d96ca6e360df07a
SNFASA <syednabil.0413@gmail.com>
2026-02-08 16:30:03 +0800

    PHP Multidimensional Array

--- backend.php
+++ backend.php
@@ -126,5 +126,35 @@ if (!empty($edit_id)) {
  * CONNECT - Establish a tunnel to the server
  * TRACE - Perform a message loop-back test along the path to the target resource
  */
+
+
+/**
+ * Multidimensional Array - An array containing one or more arrays
+ * 
+ */
+$cars = [
+    ["Volvo", 22, 18],
+    ["BMW", 15, 13],
+    ["Saab", 5, 2],
+    ["Land Rover", 17, 15],
+];
+// Accessing array using indexes
+echo $cars[0][0]."In stock: ".$cars[0][1].", sold: ".$cars[0][2]."<br/>";
+echo $cars[1][0]."In stock: ".$cars[1][1].", sold: ".$cars[1][2]."<br/>";
+echo $cars[2][0]."In stock: ".$cars[2][1].", sold: ".$cars[2][2]."<br/>";
+echo $cars[3][0]."In stock: ".$cars[3][1].", sold: ".$cars[3][2]."<br/>";
+
+
+//accessing array using loops
+for ($row = 0; $row < count($cars); $row++){
+    echo "<p><b>row number $row</b></p>";
+    echo "<ul>";
+    for ($col = 0; $col < count($cars[$row]); $col++){
+        echo "<li>".$cars[$row][$col]."</li>";
+    }
+    echo "</ul>";
+}
+
+
 ?>
 
