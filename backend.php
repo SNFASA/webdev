63028f8631aa07c19cb4337aefeeaaf9b80c0640
SNFASA <syednabil.0413@gmail.com>
2026-02-08 20:16:45 +0800

    change

--- backend.php
+++ backend.php
@@ -139,10 +139,10 @@ $cars = [
     ["Land Rover", 17, 15],
 ];
 // Accessing array using indexes
-echo $cars[0][0]."In stock: ".$cars[0][1].", sold: ".$cars[0][2]."<br/>";
-echo $cars[1][0]."In stock: ".$cars[1][1].", sold: ".$cars[1][2]."<br/>";
-echo $cars[2][0]."In stock: ".$cars[2][1].", sold: ".$cars[2][2]."<br/>";
-echo $cars[3][0]."In stock: ".$cars[3][1].", sold: ".$cars[3][2]."<br/>";
+echo $cars[0][0]." In stock: ".$cars[0][1].", sold: ".$cars[0][2]."<br/>";
+echo $cars[1][0]." In stock: ".$cars[1][1].", sold: ".$cars[1][2]."<br/>";
+echo $cars[2][0]." In stock: ".$cars[2][1].", sold: ".$cars[2][2]."<br/>";
+echo $cars[3][0]." In stock: ".$cars[3][1].", sold: ".$cars[3][2]."<br/>";
 
 
 //accessing array using loops
@@ -154,7 +154,3 @@ for ($row = 0; $row < count($cars); $row++){
     }
     echo "</ul>";
 }
-
-
-?>
-
